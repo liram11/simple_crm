1412557dce26b2f6cfa990cb6502a8acb6414f61
liram11 <liram07@gmail.com>
2024-02-16 16:32:09 +0700

    add min_employee filter

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -7,13 +7,20 @@ module Api
         companies = Company.includes(:deals).order(created_at: :desc)
 
         if filter_params[:company_name]
-          companies = companies.where("companies.name LIKE LOWER(?)", "#{filter_params[:company_name]}%")
+          companies = companies.where('companies.name LIKE LOWER(?)', "#{filter_params[:company_name]}%")
         end
 
         if filter_params[:industry]
-          companies = companies.where("companies.industry LIKE LOWER(?)", "#{filter_params[:industry]}%")
+          companies = companies.where('companies.industry LIKE LOWER(?)', "#{filter_params[:industry]}%")
         end
 
+        if filter_params[:min_employee]
+          companies = companies.where('companies.employee_count >= ?', filter_params[:min_employee])
+        end
+
+        # if filter_params[:min_deal_amount]
+        #   companies = companies.
+        # end
 
         render json: companies.as_json(include: :deals)
       end
