013a52f7dd549da6ba5e53b00bb1dc9860bfa10f
liram11 <liram07@gmail.com>
2024-02-16 16:58:10 +0700

    add filter by min_deal_amount

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -4,22 +4,23 @@ module Api
   module V1
     class CompaniesController < ApplicationController
       def index
-        companies = Company.includes(:deals).order(created_at: :desc)
+        companies = Company.with_deals_sums.order(created_at: :desc)
+        companies = filter_companies(companies)
 
-        companies = companies.filter_by_name(filter_params[:company_name]) if filter_params[:company_name].present?
-        companies = companies.filter_by_industry(filter_params[:industry]) if filter_params[:industry].present?
-        if filter_params[:min_employee].present?
-          companies = companies.filter_by_min_employee(filter_params[:min_employee])
-        end
-
-        # if filter_params[:min_deal_amount]
-        #   companies = companies.
-        # end
-
-        render json: companies.as_json(include: :deals)
+        render json: companies
       end
 
       private
+
+      # rubocop:disable Layout/LineLength, Metrics/AbcSize
+      def filter_companies(companies)
+        companies = companies.filter_by_name(filter_params[:company_name]) if filter_params[:company_name].present?
+        companies = companies.filter_by_industry(filter_params[:industry]) if filter_params[:industry].present?
+        companies = companies.filter_by_min_employee(filter_params[:min_employee]) if filter_params[:min_employee].present?
+        companies = companies.filter_by_min_deal_amount(filter_params[:min_deal_amount]) if filter_params[:min_deal_amount].present?
+        companies
+      end
+      # rubocop:enable Layout/LineLength, Metrics/AbcSize
 
       def filter_params
         params.fetch(:filters, {}).permit(
--- app/models/company.rb
+++ app/models/company.rb
@@ -3,8 +3,13 @@
 class Company < ApplicationRecord
   has_many :deals, dependent: :destroy
 
+  scope :with_deals_sums, lambda {
+    joins(:deals).group('companies.id').select('companies.*, SUM(deals.amount) as deals_sum')
+  }
+
   # filters
   scope :filter_by_name, ->(name) { where('companies.name LIKE LOWER(?)', "#{name}%") }
   scope :filter_by_industry, ->(industry) { where('companies.industry LIKE LOWER(?)', "#{industry}%") }
   scope :filter_by_min_employee, ->(min_employee) { where('companies.employee_count >= ?', min_employee) }
+  scope :filter_by_min_deal_amount, ->(min_deal_amount) { with_deals_sums.having('deals_sum >= ?', min_deal_amount) }
 end
