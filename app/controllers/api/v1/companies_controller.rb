572fb1d66aa2e2c5199eedca75c6aed8bd67f9a6
liram11 <liram07@gmail.com>
2024-02-16 16:15:24 +0700

    add spec for filters

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class CompaniesController < ApplicationController
       def index
-        companies = Company.all.order(created_at: :desc)
+        companies = Company.includes(:deals).order(created_at: :desc)
         render json: companies.as_json(include: :deals)
       end
     end
--- spec/controllers/api/v1/companies_controller_spec.rb
+++ spec/controllers/api/v1/companies_controller_spec.rb
@@ -10,6 +10,60 @@ describe Api::V1::CompaniesController do
 
         expect(response.parsed_body.map { |el| el['id'] }.sort).to eq(deals.map(&:company_id).sort)
       end
+
+      context 'filters' do
+        let!(:company) { create(:company, name: 'Super Company 1', industry: 'Some industry', employee_count: 1200) }
+        let!(:deal) { create(:deal, company:, amount: 200_000) }
+        let!(:company2) { create(:company, name: 'Super Company 2') }
+        let!(:deal2) { create(:deal, company: company2) }
+
+        it 'should filter by company name' do
+          get :index, params: { format: :json, filters: { company_name: company.name } }
+
+          expect(response.parsed_body.size).to eq(1)
+        end
+
+        it 'should filter by partial company name' do
+          get :index, params: { format: :json, filters: { company_name: company.name[0..7] } }
+
+          expect(response.parsed_body.size).to eq(2)
+        end
+
+        it 'should filter by industry' do
+          get :index, params: { format: :json, filters: { industry: company.industry } }
+
+          expect(response.parsed_body[0]['industry']).to eq(company.industry)
+        end
+
+        it 'should filter by partial industry' do
+          get :index, params: { format: :json, filters: { industry: company.industry[0...7] } }
+
+          expect(response.parsed_body[0]['industry']).to eq(company.industry)
+        end
+
+        it 'should filter by min employee count' do
+          get :index, params: { format: :json, filters: { min_employee: 1200 } }
+
+          expect(response.parsed_body.size).to eq(1)
+        end
+
+        it 'should filter by min deal amount' do
+          get :index, params: { format: :json, filters: { min_deal_amount: 190_000 } }
+
+          expect(response.parsed_body.size).to eq(1)
+        end
+
+        it 'should filter by name, indusry, min_employee and min_deal_amount simultaneously' do
+          get :index, params: { format: :json, filters: {
+            company_name: company.name,
+            industry: company.industry[0...7],
+            min_employee: 1200,
+            min_deal_amount: 190_000
+          } }
+
+          expect(response.parsed_body.size).to eq(1)
+        end
+      end
     end
   end
 end
